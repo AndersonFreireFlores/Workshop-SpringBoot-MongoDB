163879e36d9a1363cf0722c2dff30a708e79eb0f
Anderson Freire Flores <andersonfflores75@gmail.com>
2023-12-15 14:39:22 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/andersonfflores/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package andersonfflores.workshopmongo.repositories;
+
+import andersonfflores.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {}
