ed2bcbc26fb7c0ac8fadb056cef8ca90293de442
Anderson Freire Flores <andersonfflores75@gmail.com>
2023-12-16 15:06:32 -0300

    Inserção de usuário com POST

--- src/main/java/andersonfflores/workshopmongo/services/UserService.java
+++ src/main/java/andersonfflores/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package andersonfflores.workshopmongo.services;
 
 import andersonfflores.workshopmongo.domain.User;
+import andersonfflores.workshopmongo.dto.UserDTO;
 import andersonfflores.workshopmongo.repositories.UserRepository;
 import andersonfflores.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
     }
 }
