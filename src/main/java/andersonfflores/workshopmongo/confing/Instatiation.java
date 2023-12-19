9f035a48d90f7c8984aab20733f0897ce6ae0571
Anderson Freire Flores <andersonfflores75@gmail.com>
2023-12-19 13:34:24 -0300

    Acrescentando comentários aos posts

--- src/main/java/andersonfflores/workshopmongo/confing/Instatiation.java
+++ src/main/java/andersonfflores/workshopmongo/confing/Instatiation.java
@@ -3,6 +3,7 @@ package andersonfflores.workshopmongo.confing;
 import andersonfflores.workshopmongo.domain.Post;
 import andersonfflores.workshopmongo.domain.User;
 import andersonfflores.workshopmongo.dto.AuthorDTO;
+import andersonfflores.workshopmongo.dto.CommentDTO;
 import andersonfflores.workshopmongo.repositories.PostRepository;
 import andersonfflores.workshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,14 @@ public class Instatiation implements CommandLineRunner {
                 "Partiu viagem", "Vou viajar para São Paulo!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"),
                 "Bomdia", "Acordei feliz", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem", sdf.parse("21/03/2018"),new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"),new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um bom dia", sdf.parse("23/03/2018"),new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+
 
         postRepository.saveAll(Arrays.asList(post1,post2));
 
